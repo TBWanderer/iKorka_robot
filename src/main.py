17dfd85ceaa0df6eb24dffde959428eb7ef409ff
TBWanderer <evgenypodyryaka@yandex.ru>
2023-12-02 18:58:58 +0300

    Updated bot starting

--- src/main.py
+++ src/main.py
@@ -1,21 +1,70 @@
-import asyncio, os
+print("[INFO]: Importing modules...")
+
+import asyncio, time
 from aiogram import Bot, Dispatcher
 from handlers import start
-from dotenv import load_dotenv
+
+print("[INFO]: Modules imported successfuly!")
+
+
+def xor(plain_text: bytes, key: bytes):
+    pt = plain_text
+    len_key = len(key)
+    encoded = []
+    for i in range(0, len(pt)):
+        encoded.append(pt[i] ^ key[i % len_key])
+    return bytes(encoded)
+
+
+key = bytes(input("[REQUEST]: Enter key to decrypt token: "), 'utf-8')
+
+BOT_TOKEN = ""
+
+try:
+    BOT_TOKEN = xor(open("src/config.dat", "rb").read(), key).decode('utf-8')
+except Exception:
+    print("[ERROR]: Key is incorrect!")
+
+
 
 
 # Запуск бота
 async def main():
-    load_dotenv(dotenv_path="./.env")
-    print(os.getenv("BOT_TOKEN"))
-    bot = Bot(token=str(os.environ.get("BOT_TOKEN")))
-    dp = Dispatcher()
+    print("[INFO]: Bot init...")
+    try:
+        bot = Bot(token=BOT_TOKEN)
 
-    dp.include_routers(start.router)
+        print("[INFO]: Key is correct!")
 
-    await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot)
+        time.sleep(0.1)
 
+        print("[INFO]: Setting dispatcher...")
+        dp = Dispatcher()
+        print("[INFO]: Dispatcher set successfuly!")
+
+        time.sleep(0.1)
+
+        print("[INFO]: Including routers...")
+        dp.include_routers(start.router)
+        print("[INFO]: Routers included succesfuly!")
+
+        time.sleep(0.1)
+
+        print("[INFO]: Deleting webhooks...")
+        await bot.delete_webhook(drop_pending_updates=True)
+        print("[INFO]: Webhooks deleted successfuly!")
+
+        time.sleep(0.1)
+
+        print("[INFO]: Starting bot...")
+        await dp.start_polling(bot)
+
+        print("[INFO]: Deactivation bot...")
+        print("[INFO]: Bot deactivated successfuly!") 
+    except Exception as e:
+        print("[ERROR]: " + str(e))
 
 if __name__ == "__main__":
     asyncio.run(main())
+
+print("[INFO]: Exit out of program.")
